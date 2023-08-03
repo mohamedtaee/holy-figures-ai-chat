27e8b21a5a7a8ca7c3e82e971da1449807633e19
mohamedtaee <mohamed2@arizona.edu>
2023-08-03 15:05:41 -0500

    Removed unnecessary code.

--- app.py
+++ app.py
@@ -1,5 +1,3 @@
-import os
-
 from flask import Flask, render_template, request, jsonify
 from dotenv import load_dotenv
 from scripts.mongo_db import MongoDB
@@ -20,17 +18,6 @@ def ai_prompt():
     holy_book = request.args.get('holybook')
     holy_figure = request.args.get('holyfigure')
     question = request.args.get('question')
-
-    # import openai
-    # openai.api_key = os.getenv("OPENAI_API_KEY")
-    #
-    # response = openai.Completion.create(
-    #     engine="text-davinci-002",
-    #     prompt=question,
-    #     max_tokens=60
-    # )
-    #
-    # answer = response.choices[0].text.strip()
 
     openai_response = openai_prompt.ai_prompt(question=question, holybook=holy_book, holyfigure=holy_figure)
     response_id = openai_response.id
@@ -82,6 +69,7 @@ def questions_remaining():
 
     return jsonify(mongo.get_questions_remaining(userid))
 
+
 @app.route('/questions_remaining_decrement')
 def questions_remaining_decrement():
     userid = request.args.get('userid')
@@ -92,7 +80,4 @@ def questions_remaining_decrement():
 
 
 if __name__ == '__main__':
-    from waitress import serve
-
-    # serve(app, host="0.0.0.0", port=8080)
     app.run(debug=True, host='0.0.0.0')
